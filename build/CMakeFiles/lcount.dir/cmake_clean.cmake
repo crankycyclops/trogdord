68894a32e8cdcb3d3aa3ba94a1ed95f95293dc5a
James Colannino <james@colannino.dev>
2022-03-13 15:02:14 -0700

    removed build files added by mistake

--- build/CMakeFiles/lcount.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/lcount"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/lcount.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
